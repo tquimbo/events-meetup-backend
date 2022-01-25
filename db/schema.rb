3058a520e06e7bd0980f7ff1f87ee4366c6563f2
Tim Quimbo <tquimbo1@gmail.com>
2022-01-25 18:17:34 -0500

    Added User table and bcrpyt gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_04_023454) do
+ActiveRecord::Schema.define(version: 2022_01_25_231650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 2022_01_04_023454) do
     t.string "attendance_status"
     t.string "event_type"
     t.integer "seatgeek_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
