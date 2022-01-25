a40555d224619d13fa0ee00bf44a5031f07f6130
Tim Quimbo <tquimbo1@gmail.com>
2022-01-25 18:37:09 -0500

    User models and controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
-end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+          render json: user
+        else
+          render json: {errors: user.errors.full_messages.to_sentence}, status: :unprocessable_entity
+        end
+    end
+  
+  
+  
+    private
+  
+    def user_params
+      params.permit(:username, :zip_code, :password)
+    end
+  
+  end
