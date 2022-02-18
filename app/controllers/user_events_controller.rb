0992014d6321272eaf6ee8e718534b18ce34fe6d
Tim Quimbo <tquimbo1@gmail.com>
2022-02-18 00:10:15 -0500

    Added user event controller methods

--- app/controllers/user_events_controller.rb
+++ app/controllers/user_events_controller.rb
@@ -1,2 +1,19 @@
 class UserEventsController < ApplicationController
+
+
+    def create
+        if user = User.find(user_run_params[:user_id]) && event = Event.find(user_run_params[:event_id])
+            user_event = UserEvent.create(user_run_params)
+            render json: event, include: [:users]
+        else 
+            render json: { error: 'failed to add event'}, status: :not_acceptable
+        end  
+    end
+
+
+    private
+
+    def user_event_params
+        params.require(:user_event).permit(:user_id, :event_id)
+    end
 end
