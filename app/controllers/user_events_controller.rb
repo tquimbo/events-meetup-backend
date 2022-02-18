6c19454ec8248971911febe0ef89423f434bd355
Tim Quimbo <tquimbo1@gmail.com>
2022-02-18 00:58:29 -0500

    Worked on User Events Controller

--- app/controllers/user_events_controller.rb
+++ app/controllers/user_events_controller.rb
@@ -1,12 +1,21 @@
 class UserEventsController < ApplicationController
 
+    def index
+        user_events = UserEvent.all
+        render json: user_events
+    end
+
     def create
-        if user = User.find(user_run_params[:user_id]) && event = Event.find(user_run_params[:event_id])
-            user_event = UserEvent.create(user_run_params)
+        user = User.find(user_event_params[:user_id]) 
+        event = Event.find(user_event_params[:event_id])
+        if event.user_owner_id === user.id
+            render json: { error: "You already added this event!"}
+        # else
+        #     render json: { error: 'failed to add event'}, status: :not_acceptable
+        else
+            user_event = UserEvent.create(user_event_params)
             render json: event, include: [:users]
-        else 
-            render json: { error: 'failed to add event'}, status: :not_acceptable
-        end  
+        end
     end
 
 
@@ -15,7 +24,6 @@ class UserEventsController < ApplicationController
     def user_event_params
         params.require(:user_event).permit(:user_id, :event_id)
     end
-end
 
 
 end
